c0495799c2771d92247f56836257c56d7b1bc3cd
studalex <stud.alex.2302@gmail.com>
2015-09-28 05:25:58 +0300

    se1

--- javase01.t04/src/Loop2.java
+++ javase01.t04/src/Loop2.java
@@ -2,39 +2,32 @@ import java.util.Scanner;
 import static java.lang.System.*;
 
 public class Loop2 {
-    public static void main(String... arg)
-    {
+    public static void main(String... arg) {
         Scanner scanner = new Scanner(System.in);
-        int valueNumber=0;
         int[] masNumber;
-        int maxSum=0;
+        int[] sumNumber;
+        int maxSum = Integer.MIN_VALUE;
 
-        out.print("Enter N: ");
+        out.print("Enter the sequence of elements separated by spaces: ");
+        String str = "";
+        str = scanner.nextLine();
 
-        boolean noError = false;
-        do {
-            try {
-                valueNumber = Integer.parseInt(scanner.next());
-                noError = true;
-            } catch (Exception exeption) {
-                out.println("Is not a number, try again: ");
-                noError = false;
-            }
-        } while (!noError);
+        String[] masStr = str.split(" ");
+        masNumber = new int[masStr.length];
+        sumNumber = new int[masStr.length];
 
-        if(valueNumber<0) valueNumber = Math.abs(valueNumber);
-
-        masNumber = new int[valueNumber];
-
-        for(int i=0; i<valueNumber; i++)
-        {
-            masNumber[i] = i+1 + (valueNumber*2-i);
-            if(maxSum < masNumber[i])
-            {
-                maxSum = masNumber[i];
-            }
-            out.println("Sum A(" + (i + 1) + ") + A(" + (valueNumber * 2 - i) + ") =: " + masNumber[i]);
+        for (int i = 0; i < masStr.length; i++) {
+            masNumber[i] = Integer.parseInt(masStr[i]);
         }
+
+        for (int i = 0; i < masNumber.length; i++) {
+            sumNumber[i] = masNumber[i] + masNumber[masNumber.length - i - 1];
+            out.println(masNumber[i] + " + " + masNumber[masNumber.length - i - 1] + " = " + sumNumber[i]);
+            if (maxSum < sumNumber[i]) {
+                maxSum = sumNumber[i];
+            }
+        }
+
         out.println("Maximum = " + maxSum);
         scanner.close();
     }
