f4b9413b0a76256faf779191d13f6ad876e5ad14
studalex <stud.alex.2302@gmail.com>
2015-10-04 12:37:16 +0300

    se1

---
+++ javase01.t06/src/Entry.java
@@ -0,0 +1,37 @@
+
+/**
+ * Класс хранит запись блокнота.
+ */
+class Entry {
+    /**
+     * Переменная харнит запись блокнота.
+     */
+    private String entry;
+
+    /**
+     * Конструктор класса.
+     *
+     * @param newEntry Строка, которая будет добавленна в класс.
+     */
+    public Entry(String newEntry) {
+        entry = newEntry;
+    }
+
+    /**
+     * Изменить старую запись.
+     *
+     * @param newEntry Новая запись.
+     */
+    void setEntry(String newEntry) {
+        entry = newEntry;
+    }
+
+    /**
+     * Возвращает запись.
+     *
+     * @return запись
+     */
+    String getEntry() {
+        return entry;
+    }
+}
---
+++ javase01.t06/src/Main.java
@@ -0,0 +1,37 @@
+import static java.lang.System.*;
+
+public class Main {
+    public static void main(String... arg) {
+        Notebook MyNotebook = new Notebook();
+
+        MyNotebook.addNewEntry("Первая запись в блокноте");
+        MyNotebook.addNewEntry("Вторая запись в блокноте");
+        MyNotebook.addNewEntry("Третья запись в блокноте");
+        MyNotebook.addNewEntry("Четвертая запись в блокноте");
+        out.println();
+
+        MyNotebook.printAllEntry();
+        out.println();
+
+        MyNotebook.editEntry(3, "Третья запись в блокноте отредактирована");
+        MyNotebook.printAllEntry();
+        out.println();
+
+        MyNotebook.deleteEntry(2);
+        MyNotebook.printAllEntry();
+        out.println();
+
+        out.println("Попытка удалить несуществующую запись");
+        MyNotebook.deleteEntry(4); //В рамках сущестующего массива
+        MyNotebook.deleteEntry(6); //Вне массива
+        MyNotebook.printAllEntry();
+        out.println();
+
+        out.println("Попытка изменить несуществующую запись");
+        MyNotebook.editEntry(4, "Четвертая запись, изменись!"); //В рамках сущестующего массива
+        MyNotebook.editEntry(6, "Четвертая запись, изменись!"); //Вне массива
+        MyNotebook.printAllEntry();
+    }
+}
+
+
